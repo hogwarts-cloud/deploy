3ad57314fa0c34148b6d7759478c5317b0eadd0c
Danil Kazakov <cmaster057@gmail.com>
2024-05-21 10:50:47 +0500

    improve ansible

--- export.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-yc config profile activate hogwarts-cloud
-export YC_TOKEN=$(yc iam create-token)
-export YC_CLOUD_ID=$(yc config get cloud-id)
-export YC_FOLDER_ID=$(yc config get folder-id)
